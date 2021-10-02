de02ec824b492f95e2246c8e22458f9224222f30
tina <3310156392@qq.com>
2021-10-02 15:42:19 +0800

    development

---
+++ demos/development/src/print.js
@@ -0,0 +1,3 @@
+export default function print() {
+  consoel.log('I got called from print.js');
+}
---
+++ demos/development/webpack-devserver.config.js
@@ -0,0 +1,24 @@
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: Path.resolve(__dirname, './src/index.js'),
+    // print: Path.resolve(__dirname, './src/print.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: Path.resolve(__dirname, './dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '开发环境'
+    })
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  }
+};
